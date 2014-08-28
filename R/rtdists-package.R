a57c35219583fc54d6709ca62b15c74d462ae9af
singmann <henrik.singmann@psychologie.uni-freiburg.de>
2014-08-28 13:43:43 +0200

    added roxyPackage for roxygenizing documentation

---
+++ R/rtdists-package.R
@@ -0,0 +1,21 @@
+#' Respone Time distribtuions in R.
+#'
+#' \tabular{ll}{
+#' Package: \tab rtdists\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1-0\cr
+#' Date: \tab 2014-08-28\cr
+#' Depends: \tab R (>= 3.0.0)\cr
+#' License: \tab GPL (>=3)\cr
+#' URL: \tab https://github.com/rtdists/rtdists/\cr
+#' }
+#'
+#' Provides response time distributions based on C code by Andreas Voss.
+#'
+#' @aliases rtdists-package
+#' @name rtdists-package
+#' @docType package
+#' @title The rtdists Package
+#' @author Andrew Heathcote, Matthew Gretton, Andreas Voss, Henrik Singmann
+#' @keywords package
+NULL
--- development.R
+++ development.R
@@ -6,3 +6,35 @@ rrd(10, c(rep(1, 4), rep(0.1, 4)))
 
 require(testthat)
 test_package("rtdists")
+
+
+
+require(roxyPackage)  # install.packages("roxyPackage", repo="http://R.reaktanz.de")
+
+# Linux:
+R.libs <- "."
+
+roxy.package(
+  pck.source.dir = ".",
+  pck.version = "0.1-0",
+  pck.description = data.frame(
+    Package = "rtdists",
+    Type = "Package",
+    Title = "Respone Time distribtuions in R",
+    AuthorsR = "c(
+        person(given=\"Andrew\", family=\"Heathcote\", role=c(\"aut\")),
+        person(given=\"Matthew\", family=\"Gretton\", role=c(\"aut\")),
+        person(given=\"Andreas\", family=\"Voss\", role=c(\"aut\")),
+        person(given=\"Henrik\", family=\"Singmann\", email=\"singmann+rtdists@gmail.com\", role=c(\"aut\", \"cre\"))
+    )",
+    Depends = "R (>= 3.0.0)",
+    Suggests = "testthat",
+    #Imports = "",
+    Description = "Provides response time distributions based on C code by Andreas Voss.",
+    URL = "https://github.com/rtdists/rtdists/",
+    License = "GPL (>=3)",
+    stringsAsFactors = FALSE),
+  actions = c("roxy"),
+  R.libs = R.libs, 
+  repo.root = tempdir())
+
