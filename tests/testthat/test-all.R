d22508fe725d73484b97dbe43f7eebdde7ca63f2
singmann <henrik.singmann@psychologie.uni-freiburg.de>
2014-08-30 19:28:59 +0200

    tests (testthat) run with R CMD check

--- tests/testthat/test-all.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(rtdists)
-
-test_package("rtdists")
--- tests/testthat/test-rrd.R
+++ tests/testthat/test-rrd.R
@@ -2,5 +2,5 @@
 context("rrd: random number generation for the diffusion model")
 
 test_that("rrd works", {
-  expect_that(rrd(10, c(rep(1, 4), rep(0.1, 4))), is_a("data.frame"))
+  expect_that(rrd(10, c(rep(1, 4), rep(0.1, 4))), is_a("matrix"))
 })
