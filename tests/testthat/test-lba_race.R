af6c84426513baa2eab36a28e1b6389953187245
singmann <henrik.singmann@psychologie.uni-freiburg.de>
2014-11-27 12:06:23 +0100

    increased test size

--- tests/testthat/test-lba_race.R
+++ tests/testthat/test-lba_race.R
@@ -3,8 +3,8 @@ context("LBA race functions")
 
 test_that("n1CDF corresponds to random derivates", {
   normalised_n1CDF = function(t,...) n1CDF(t,...)/n1CDF(t=Inf,...) 
-  r_lba1 <- rlba_norm(1e3, A=0.5, b=1, t0 = 0.5, mean_v=c(1.2, 1), sd_v=0.2)
-  r_lba2 <- rlba_norm(1e3, A=0.5, b=1, t0 = 0.5, st0 = 0.2, mean_v=c(1.2, 1), sd_v=0.2)
+  r_lba1 <- rlba_norm(2e3, A=0.5, b=1, t0 = 0.5, mean_v=c(1.2, 1), sd_v=0.2)
+  r_lba2 <- rlba_norm(2e3, A=0.5, b=1, t0 = 0.5, st0 = 0.2, mean_v=c(1.2, 1), sd_v=0.2)
   expect_less_than(ks.test(r_lba1$rt[r_lba1$response==1], "n1CDF", A=0.5, b=1, t0 = 0.5, mean_v=c(1.2, 1.0), sd_v=0.2)$p.value, 0.0001)
   expect_less_than(ks.test(r_lba1$rt[r_lba1$response==1], normalised_n1CDF, A=0.6, b=1, t0 = 0.5, mean_v=c(1.2, 1.0), sd_v=0.2)$p.value, 0.0001)
   expect_less_than(ks.test(r_lba2$rt[r_lba2$response==1], normalised_n1CDF, A=0.5, b=1, t0 = 0.5, mean_v=c(1.2, 1.0), sd_v=0.2)$p.value, 0.0001)
