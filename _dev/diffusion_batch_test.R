2457f56a71b1f0de05ff364ec912e58347ff1c85
Matthew Gretton <matthew.gretton@utas.edu.au>
2015-07-05 16:41:13 +1000

    This probably kills the R.

---
+++ _dev/diffusion_batch_test.R
@@ -0,0 +1,189 @@
+#' The Ratcliff Diffusion Model
+#' 
+#' Density, distribution function, and random generation for the Ratcliff diffusion model with eight parameters: \code{a} (threshold separation), \code{z} (relative starting point), \code{v} (drift rate), \code{t0} (non-decision time/response time constant), \code{d} (differences in speed of response execution), \code{sv} (inter-trial-variability of drift), \code{st0} (inter-trial-variability of non-decisional components), and \code{sz} (inter-trial-variability of relative starting point). 
+#'
+#' @param t a vector of RTs.
+#' @param n desired number of observations.
+#' @param boundary character vector. Which boundary should be tested. Possible values are \code{c("upper", "lower")}, possibly abbreviated and \code{"upper"} being the default.
+#' 
+#' @param a threshold separation. Amount of information that is considered for a decision. Large values indicate a conservative decisional style. Typical range: 0.5 < \code{a} < 2
+#' @param v drift rate. Average slope of the information accumulation process. The drift gives information about the speed and direction of the accumulation of information. Large (absolute) values of drift indicate a good performance. If received information supports the response linked to the upper threshold the sign will be positive and vice versa. Typical range: -5 < \code{v} < 5
+#' @param t0 non-decision time or response time constant (in seconds). Lower bound for the duration of all non-decisional processes (encoding and response execution). Typical range: 0.1 < \code{t0} < 0.5
+#' 
+#' @param z relative starting point. Indicator of an a priori bias in decision making. When the relative starting point \code{z} deviates from 0.5, the amount of information necessary for a decision differs between response alternatives. Typical range: 0.3 < \code{z} < 0.7. Default is 0.5 (i.e., no bias).
+#' @param d differences in speed of response execution (in seconds). Positive values indicate that response execution is faster for responses linked to the upper threshold than for responses linked to the lower threshold. Typical range: -0.1 < \code{d} < 0.1. Default is 0.
+#' @param sz inter-trial-variability of (relative) starting point. Range of a uniform distribution with mean \code{z} describing the distribution of actual starting points from specific trials. Minimal impact on the RT distributions. Can be fixed to 0 in most applications. Typical range: 0 < \code{sz} < 0.5. Default is 0.
+#' @param sv inter-trial-variability of drift rate. Standard deviation of a normal distribution with mean \code{v} describing the distribution of actual drift rates from specific trials. 	Minimal impact on the RT distributions. Can be fixed to 0 in most applications. Typical range: 0 < \code{sv} < 2. Default is 0.
+#' @param st0 inter-trial-variability of non-decisional components. Range of a uniform distribution with mean \code{t0 + st0/2} describing the distribution of actual \code{t0} values across trials. Accounts for response times below \code{t0}. Reduces skew of predicted RT distributions. Typical range: 0 < \code{st0} < 0.2. Default is 0.
+#' 
+#' @param precision \code{numerical} scalar value. Precision of calculation. Corresponds roughly to the number of decimals of the predicted CDFs that are calculated accurately. Default is 3.
+#' @param maxt maximum \code{rt} allowed, used to stop integration problems (\code{prd} only).
+#'
+#' @return \code{drd} gives the density, \code{prd} gives the distribution function, and \code{rrd} generates random response times and decisions (returning a \code{data.frame} with columns \code{rts} (numeric) and \code{response} (factor)).
+#'
+#' @details The Ratcliff diffusion model (Ratcliff, 1978) is a mathematical model for two-choice discrimination tasks. It is based on the assumption that information is accumulated continuously until one of two decision thresholds is hit. For more information, see Voss, Rothermund, and Voss (2004), Voss, Nagler, and Lerche (2013), or Wagenmakers (2009).
+#' 
+#' @references Ratcliff, R. (1978). A theory of memory retrieval. \emph{Psychological Review}, 85(2), 59-108.
+#' 
+#' Voss, A., Rothermund, K., & Voss, J. (2004). Interpreting the parameters of the diffusion model: An empirical validation. \emph{Memory & Cognition}. Vol 32(7), 32, 1206-1220.
+#' 
+#' Voss, A., Nagler, M., & Lerche, V. (2013). Diffusion Models in Experimental Psychology: A Practical Introduction. \emph{Experimental Psychology}, 60(6), 385-402. doi:10.1027/1618-3169/a000218
+#' 
+#' Wagenmakers, E.-J. (2009). Methodological and empirical developments for the Ratcliff diffusion model of response times and accuracy. \emph{European Journal of Cognitive Psychology}, 21(5), 641-671.
+#' 
+#' 
+#' @author Underlying C code by Jochen Voss and Andreas Voss. Porting and R wrapping by Matthew Gretton, Andrew Heathcote, and Henrik Singmann.
+#'
+#' @useDynLib rtdists, dfastdm_b, pfastdm_b, rfastdm
+#'
+#' @name Diffusion
+#' 
+#' @example examples/examples.diffusion.R
+#' 
+NULL
+
+#pl <- get_rd_parameters(a = a, z = z, v = v, t0 = t0, d = d, sz = sz, sv = sv, st0 = st0, pl = parameters)
+# get_rd_parameters <- function(a, z, v, t0, d, sz, sv, st0, pl) {
+#   miss_p_indiv <- c(missing(a), missing(v), missing(t0), missing(z), missing(d), missing(sz), missing(sv), missing(st0))
+#   miss_pl <- missing(pl)
+#   if(all(miss_p_indiv) & miss_pl) stop("No parameter values specified.")
+#   if(any(!miss_p_indiv) & !miss_pl) stop("Parameter values need to be specified either via individual arguments or via 'pl', but not both.")
+#   if((sum(miss_p_indiv) > 0) & miss_pl) stop("Not all parameter values specified via individual arguments.")
+#   if(miss_pl) {
+#     rd_parameters <- c(a,v,t0,d,sz,sv,st0,z)
+#     if(length(rd_parameters)!=8) stop("Each parameter needs to be of length 1.")
+#     if(!is.numeric(rd_parameters)) stop("Parameters need to be numeric.")
+#     if (any(is.na(rd_parameters)) || !all(is.finite(rd_parameters))) stop("Parameters need to be numeric and finite.")
+#     return(rd_parameters)
+#   } else {
+#     if (!is.numeric(pl) | !is.vector(pl)) stop("pl needs to be a numeric vector.")
+#     if(length(pl)!=8) stop("Length of pl is incorrect (must be 8).")
+#     if (any(is.na(pl)) || !all(is.finite(pl))) stop("Parameters need to be numeric and finite.")
+#     if (!is.null(names(pl))) {
+#       if(!all(sapply(c("a","v","t0","d","sz","sv","st0","z"), function(x) x %in% names(pl)))) stop("names(pl) not complete. Need to be: c(\"a\",\"v\",\"t0\",\"z\",\"d\",\"sz\",\"sv\",\"st0\")")
+#       pl <- pl[c("a","v","t0","d","sz","sv","st0","z")]
+#     } else {
+#       names(pl) <- c("a","z","v","t0","d","sz","sv","st0")
+#       pl <- pl[c("a","v","t0","d","sz","sv","st0","z")]
+#     }
+#     return(pl)
+#   }
+# }
+#
+
+
+
+
+
+# [MG 20150616]
+# In line with LBA, adjust t0 to be the lower bound of the non-decision time distribution rather than the average 
+# Called from prd, drd, rrd 
+recalc_t0 <- function (t0, st0) { t0 <- t0 + st0/2 }
+
+
+#' @rdname Diffusion
+#' @export drd
+drd <- function (t, boundary = c("upper", "lower"), 
+                 a, v, t0, z = 0.5, d = 0, sz = 0, sv = 0, st0 = 0, 
+                 precision = 3)
+{
+  t0 <- recalc_t0 (t0, st0) 
+  
+  # Check for illegal parameter values
+  if(any(missing(a), missing(v), missing(t0))) stop("a, v, and/ot t0 must be supplied")
+  pl <- c(a,v,t0,d,sz,sv,st0,z)
+  if(length(pl)!=8) stop("Each parameter needs to be of length 1.")
+  if(!is.numeric(pl)) stop("Parameters need to be numeric.")
+  if (any(is.na(pl)) || !all(is.finite(pl))) stop("Parameters need to be numeric and finite.")
+  
+  boundary <- match.arg(boundary)
+  if (boundary == "upper") i <- 2L
+  if (boundary == "lower") i <- 1L
+  
+  # Call the C code
+  densities <- vector(length=length(t))    
+  output <- .C("dfastdm_b", 
+               as.integer (length(t)),                 # 1  IN:  number of densities
+               as.vector  (pl),                        # 2  IN:  parameters
+               as.vector  (t),                         # 3  IN:  RTs
+               as.double  (precision),                 # 4  IN:  precision
+               as.integer (i),                         # 5  IN:  boundart 
+               as.vector  (densities, mode="numeric")  # 6 OUT:  densities
+  )
+  
+  unlist(output[6])
+  
+}
+
+#' @rdname Diffusion
+#' @export prd
+# set maximum t value to stop integration problems
+prd <- function (t, boundary = c("upper", "lower"), 
+                 a, v, t0, z = 0.5, d = 0, sz = 0, sv = 0, st0 = 0, 
+                 precision = 3, maxt = 1e4) 
+{
+  t0 <- recalc_t0 (t0, st0) 
+
+  # Check for illegal parameter values
+  if(any(missing(a), missing(v), missing(t0))) stop("a, v, and/ot t0 must be supplied")
+  pl <- c(a,v,t0,d,sz,sv,st0,z)
+  if(length(pl)!=8) stop("Each parameter needs to be of length 1.")
+  if(!is.numeric(pl)) stop("Parameters need to be numeric.")
+  if (any(is.na(pl)) || !all(is.finite(pl))) stop("Parameters need to be numeric and finite.")
+
+  t[t>maxt] <- maxt
+  if(!all(t == sort(t)))  stop("t needs to be sorted")
+  
+  boundary <- match.arg(boundary)
+  if (boundary == "upper") i <- 2L
+  if (boundary == "lower") i <- 1L
+  
+  # Call the C code
+  pvalues <- vector(length=length(t))    
+  output <- .C("pfastdm_b", 
+               as.integer (length(t)),               # 1  IN:  number of densities
+               as.vector  (pl),                      # 2  IN:  parameters
+               as.vector  (t),                       # 3  IN:  RTs
+               as.double  (precision),               # 4  IN:  number of densities
+               as.integer (i),                       # 5  IN:  boundary 
+               as.vector  (pvalues, mode="numeric")  # 6 OUT:  pvalues
+  )
+  
+  unlist(output[6])
+  
+}
+
+
+#' @rdname Diffusion
+#' @export rrd
+# Returns a matrix of 2 x n (RTs x boundaries)
+rrd <- function (n, 
+                 a, v, t0, z = 0.5, d = 0, sz = 0, sv = 0, st0 = 0, 
+                 precision = 3)
+{
+  randRTs    <- vector(length=n)
+  randBounds <- vector(length=n)
+
+  t0 <- recalc_t0 (t0, st0) 
+  
+  # Check for illegal parameter values
+  if(any(missing(a), missing(v), missing(t0))) stop("a, v, and/ot t0 must be supplied")
+  pl <- c(a,v,t0,d,sz,sv,st0,z)
+  if(length(pl)!=8) stop("Each parameter needs to be of length 1.")
+  if(!is.numeric(pl)) stop("Parameters need to be numeric.")
+  if (any(is.na(pl)) || !all(is.finite(pl))) stop("Parameters need to be numeric and finite.")
+
+  output <- .C("rfastdm", 
+               as.integer (n),                          # 1  IN:  number of densities
+               as.vector  (pl),                         # 2  IN:  parameters
+               as.double  (precision),                  # 3  IN:  precision
+               as.vector  (randRTs, mode="numeric"),    # 4 OUT:  RTs 
+               as.vector  (randBounds, mode="numeric")  # 5 OUT:  bounds 
+  )
+  
+  randRTs <- unlist(output[4]) 
+  randBounds <- unlist(output[5])
+  response <- factor(randBounds, levels = 0:1, labels = c("lower", "upper"))
+  data.frame(rt = randRTs, response)
+}
+
--- _dev/rfastdm vectorise test 2.R
+++ _dev/rfastdm vectorise test 2.R
@@ -5,104 +5,119 @@ library (devtools)
 devtools::install_git ("https://github.com/rtdists/rtdists.git")
 library (rtdists)
 
+source ("_dev/diffusion_batch_test.R")
 
-#' @rdname Diffusion
-#' @export prd
-# set maximum t value to stop integration problems
-prd_test <- function (t, boundary = c("upper", "lower"), 
-                 a, v, t0, z = 0.5, d = 0, sz = 0, sv = 0, st0 = 0, 
-                 precision = 3, maxt = 1e4) 
-{
-  t0 <- recalc_t0 (t0, st0) # [MG 20150616] Call recalc_t0
-  
-  # Check for illegal parameter values
-  if(any(missing(a), missing(v), missing(t0))) stop("a, v, and/ot t0 must be supplied")
-  pl <- c(a,v,t0,d,sz,sv,st0,z)
-  
-  length(pl)
-  
-  if(length(pl)!=8) stop("Each parameter needs to be of length 1.")
-  if(!is.numeric(pl)) stop("Parameters need to be numeric.")
-  if (any(is.na(pl)) || !all(is.finite(pl))) stop("Parameters need to be numeric and finite.")
-  
-  t[t>maxt] <- maxt
-  if(!all(t == sort(t)))  stop("t needs to be sorted")
-  
-  boundary <- match.arg(boundary)
-  if (boundary == "upper") i <- 2L
-  if (boundary == "lower") i <- 1L
-  
-  # Call the C code
-  pvalues <- vector(length=length(t))    
-  output <- .C("pfastdm_b", 
-               as.integer (length(t)),               # 1  IN:  number of densities
-               as.vector  (pl),                      # 2  IN:  parameters
-               as.vector  (t),                       # 3  IN:  RTs
-               as.double  (precision),               # 4  IN:  number of densities
-               as.integer (i),                       # 5  IN:  boundary 
-               as.vector  (pvalues, mode="numeric")  # 6 OUT:  pvalues
-  )
-  
-  unlist(output[6])
-  
-}
+# VECTOR VERSION: Set up parameters
+test_vec_len <- 1
+vec_rts <- seq (1, 2.9, by=0.2)
+vec_bounds <- "upper"
+
+vec_a   <- 1
+vec_z   <- .20 
+vec_v   <- .30
+vec_t0  <- .40
+vec_d   <- .50
+vec_sz  <- .06
+vec_sv  <- .70
+vec_st0 <- .80
+
+# MATRIX VERSION: Set up parameter vectors
+test_vec_len <- 10
+vec_rts <- seq (1, 2.9, by=0.2)
+vec_bounds <- sample (c("upper","lower"), test_vec_len, replace=TRUE)
+
+vec_a   <- c(1,    1,    1,    2,    3,    3,    3,    4,    4,    4    )
+vec_z   <- rep (.20, test_vec_len)
+vec_v   <- rep (.30, test_vec_len)
+vec_t0  <- rep (.40, test_vec_len)
+vec_d   <- rep (.50, test_vec_len)
+vec_sz  <- rep (.06, test_vec_len)
+vec_sv  <- rep (.70, test_vec_len)
+vec_st0 <- rep (.80, test_vec_len)
 
 
-params <- matrix (nrow=10, ncol=8)
-#                   a    z      v    t0     d     sz    sv   st0
-params[ 1,] <- c( 1,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 2,] <- c( 1,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 3,] <- c( 1,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 4,] <- c( 2,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 5,] <- c( 3,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 6,] <- c( 3,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 7,] <- c( 3,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 8,] <- c( 4,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[ 9,] <- c( 4,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-params[10,] <- c( 4,   0.2,   0.3,  0.4,  0.5,  0.06,  0.7,  0.8)
-
-rts <- seq (1, 2.9, by=0.2)
-
+# !! DEBUG ONLY
 # Get known right answers
-prd_vals <- vector(length=10)
-drd_vals <- vector(length=10)
-for (i in 1:10)
+#
+correct_prd_vals <- vector(length=test_vec_len)
+correct_drd_vals <- vector(length=test_vec_len)
+for (i in 1:test_vec_len)
 {
-  prd_vals[i] <- prd(rts[i], a =params[i,1], z=params[i,2], v =params[i,3], 
-                             t0=params[i,4], d=params[i,5], sz =params[i,6], 
-                             sv=params[i,7], st0 = params[i,8])
-  drd_vals[i] <- drd(rts[i], a =params[i,1], z=params[i,2], v =params[i,3], 
-                             t0=params[i,4], d=params[i,5], sz =params[i,6], 
-                             sv=params[i,7], st0 = params[i,8])
+  correct_prd_vals[i] <- prd(vec_rts[i], boundary=vec_bounds[i], a =vec_a[i], z=vec_z[i], v =vec_v[i], 
+                             t0=vec_t0[i], d=vec_d[i], sz =vec_sz[i], sv=vec_sv[i], st0 = vec_st0[i])
+  correct_drd_vals[i] <- drd(vec_rts[i], boundary=vec_bounds[i], a =vec_a[i], z=vec_z[i], v =vec_v[i], 
+                     t0=vec_t0[i], d=vec_d[i], sz =vec_sz[i], sv=vec_sv[i], st0 = vec_st0[i])
 }  
-  
+
+
+# drd (vec_rts, boundary=vec_bounds, vec_a, vec_z, vec_v, vec_t0, vec_d, vec_sz, vec_sv, vec_st0)
+t        <- vec_rts
+boundary <- vec_bounds
+
+a        <- vec_a
+v        <- vec_v
+t0       <- vec_t0
+z        <- vec_z
+d        <- vec_d
+sz       <- vec_sz
+sv       <- vec_sv
+st0      <- vec_st0
 
 
 ### START OF FUNCTION
 
-if (dim (params) == NULL)
-{
-   # This is just a single vector of parameters 
-  output <- prd_test (rts, a =params[1], z=params[2], v =params[3], 
-                           t0=params[4], d=params[5], sz =params[6], 
-                           sv=params[7], st0 = params[8])
+# Checks to add:
+#   1. Are all vectors the same length?
+#   2. If not, what do we do? 
+#      ? Cycle parameter vectors over rts vector?
+#      ? Allow some params to be scalar? (we'll need to use something as a canonical parameter length?)
+
+
+# Build the parameter matrix
+
+# Convert boundaries to numeric
+numeric_bounds <- vector (mode="numeric")
+for (i in 1:length(boundary)) { 
+  if (boundary[i] == "upper") numeric_bounds[i] <- 2L
+  if (boundary[i] == "lower") numeric_bounds[i] <- 1L
 }
-else
+
+params <- cbind (a, v, t0, z, d, sz, sv, st0, numeric_bounds)
+
+# Check for illegal parameter values
+if(any(missing(a), missing(v), missing(t0))) stop("a, v, and/ot t0 must be supplied")
+if(!is.numeric(params)) stop("Parameters need to be numeric.")
+if (any(is.na(params)) || !all(is.finite(params))) stop("Parameters need to be numeric and finite.")
+
+densities <- vector(length=length(t))    
+
+if (length(params[,1]) == 1)
 {
+  # Call the C code
+  output <- .C("dfastdm_b", 
+               as.integer (length(t)),                 # 1  IN:  number of densities
+               as.vector  (pl),                        # 2  IN:  parameters
+               as.vector  (t),                         # 3  IN:  RTs
+               as.double  (precision),                 # 4  IN:  precision
+               as.integer (i),                         # 5  IN:  boundary
+               as.vector  (densities, mode="numeric")  # 6 OUT:  densities
+  )
+  
+  unlist(output[6])
+  
+} else
+{
+  cat ("Doing vector params,\n")
   # Check matrix-specific errors
-  #  Needs to be 2D, with second dimension of length 8
-  if (length(dim(params)) != 2)   { stop("Bugger 1.") }
-  if (length (params[1,]) != 8)   { stop("Bugger 2.") }
+  #  Needs to be 2D, with second dimension of length 9 (incl. boundary)
+  if (length(dim(params)) != 2)   { stop("Needs to be a two-dimensional parameter matrix.") }
+  if (length (params[1,]) != 9)   { stop("Incorrect length of parameter rows") }
 
   uniques <- unique(params)
   unique_rows <- length(uniques[,1])
 
   # ?TODO: If we wanted to do an optimising heuristic with the percent of unique rows, we'd do it here
  
-  # Set up a vector of output values
-  output <- vector (length=length(params[,1]))
-#   output2 <- vector (length=length(params[,1]))
-  
   for (i in 1:unique_rows)
   {
     cat ("Processing unique row", i, "\n")
@@ -110,19 +125,29 @@ else
     # Get list of row indices equal to the i'th unique row
     # Todo: check this actually works!
     ok_rows <- which(params[,1]      == uniques[i,1])
-    for (j in 2:8) { ok_rows <- ok_rows[which(params[ok_rows,j] == uniques[i,j])] }    
+    for (j in 2:9) { ok_rows <- ok_rows[which(params[ok_rows,j] == uniques[i,j])] }    
     
     # Select the correct RT indices and parameters for this batch of 'ok' parameter rows
-    current_params <- params[ok_rows[1],]
+    ok_params <- params[ok_rows[1],]
 
     cat ("Rows to process:", ok_rows, "\n")
-    cat ("RTs to process:", rts[ok_rows], "\n")
-    cat ("Params to use:", current_params, "\n")
-    out <- prd_test (rts[ok_rows], a =current_params[1], z  =current_params[2], v =current_params[3], 
-                     t0=current_params[4], d  =current_params[5], sz=current_params[6], 
-                     sv=current_params[7], st0=current_params[8])
-      
-    output[ok_rows] <- out
+    cat ("RTs to process:", t[ok_rows], "\n")
+    cat ("Params to use:", ok_params, "\n")
+    
+    # pl <- c(a,v,t0,d,sz,sv,st0,z) (need to put z at the end)
+    pl <- c(ok_params[1], ok_params[2], ok_params[3], ok_params[5], 
+            ok_params[6], ok_params[7], ok_params[8], ok_params[4])  
+
+    # Call the C code
+    output <- .C("dfastdm_b", 
+                 as.integer (length(rts[ok_rows])),                 # 1  IN:  number of densities
+                 as.vector  (pl),                        # 2  IN:  parameters
+                 as.vector  (t[ok_rows]),                         # 3  IN:  RTs
+                 as.double  (precision),                 # 4  IN:  precision
+                 as.integer (ok_paramsi),                         # 5  IN:  boundary
+                 as.vector  (densities, mode="numeric")  # 6 OUT:  densities
+    )
+    densities[ok_rows] <- out
 
   
 #     out2 <- drd (rts[ok_rows], a =current_params[1], z  =current_params[2], v =current_params[3], 
@@ -131,10 +156,10 @@ else
 #     
 #     output2[ok_rows] <- out2
   }
-  output
+  densities
   
   # Test
-  all.equal (prd_vals, output)
+  all.equal (correct_prd_vals, densities)
   
 }
 
