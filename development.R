2d7ecb1293280dac0c264f749d41bf3b7504f88d
singmann <singmann@gmail.com>
2016-06-27 11:30:44 +0200

    cleaned development.R

--- development.R
+++ development.R
@@ -1,5 +1,6 @@
 
 require(devtools)
+require(testthat)
 load_all()
 
 options(error = recover)
@@ -7,23 +8,6 @@ options(error = NULL)
 
 build_vignettes()
 
-rrd(10, a=1, z=0.5, v=2, t0=0.5, d=0, sz=0, sv=0, st0=0)
-rt1 <- rrd(20, a=c(1, 1.5, 1.2), z=0.5, v=1, t0=0.5, d=0, sz=0, sv=0, st0=0)
-drd(rt1$rt, a=c(1, 1.5, 1.2), z=0.5, v=1, t0=0.5, d=0, sz=0, sv=0, st0=0, boundary = as.character(rt1$response))
-prd(sort(rt1[rt1$response == "upper", "rt"]), a=c(1, 1.5, 1.2), z=0.5, v=1, t0=0.5, d=0, sz=0, sv=0, st0=0)
-
-rlba_norm(10, A=0.5, b=1, t0 = 0.5, mean_v=c(1.2, 1), sd_v=c(0.2, 0.4, 0.5))
-rlba_gamma(10, A=0.5, b=1, t0 = 0.5, shape_v=c(1, 1.6, 0.7), scale_v=c(0.2,0.3))
-rlba_frechet(10, A=0.5, b=1, t0 = 0.5, shape_v=c(1.2, 1), scale_v=c(0.2,0.3,0.8))
-rlba_lnorm(10, A=0.5, b=1, t0 = 0.5, meanlog_v=c(1.2, 1), sdlog_v=c(0.2, 0.3,0.4))
-
-dlba_frechet(seq(0.6,2,0.1), A=A, b=b, t0=t0, shape_v=1.1, scale_v=1)
-
-rlba_norm(100, b = 1.3, A = 1, vs = c(0.8, 1.2), s = 1.2, t0 = .2, st0 = 0)
-
-(x <- rlba_frechet(10, A=0.7, b = 0.5, t0 = 0.1, shape_v = c(0.7, 1.0), scale_v = c(0.7, 1.2)))
-
-require(testthat)
 test()
 test_package("rtdists")  # no long tests of n1 functions
 test(filter = "bugs")
